1fe07cbed9c3cd10b5e16e8ed7bb03ddfb87c0c3
松本　良太 <ryouta@matsumotoryouta-no-MacBook-Pro.local>
2022-02-09 19:13:58 +0900

    スケジュール管理アプリ実装

---
+++ db/migrate/20220131125801_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.datetime :start_at
+      t.datetime :end_at default: false,
+      t.boolean :is_all_day
+
+      t.timestamps
+    end
+  end
+end
