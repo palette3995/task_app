3c50e54b5cc49e6d7b83a5e9486c5a51a871c89c
松本　良太 <ryouta@matsumotoryouta-no-MacBook-Pro.local>
2022-02-11 16:54:33 +0900

    マイグレーションファイル修正

--- db/migrate/20220131125801_create_posts.rb
+++ db/migrate/20220131125801_create_posts.rb
@@ -3,7 +3,7 @@ class CreatePosts < ActiveRecord::Migration[6.1]
     create_table :posts do |t|
       t.string :title
       t.datetime :start_at
-      t.datetime :end_at default: false,
+      t.datetime :end_at
       t.boolean :is_all_day
 
       t.timestamps
